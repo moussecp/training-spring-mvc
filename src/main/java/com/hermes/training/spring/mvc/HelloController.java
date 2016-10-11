a6ce72881d10f6c5f304c9f0b32becf70dad09f8
moussecp <yannick8@hotmail.com>
2016-10-12 00:31:03 +0200

    first commit

---
+++ src/main/java/com/hermes/training/spring/mvc/HelloController.java
@@ -0,0 +1,18 @@
+package com.hermes.training.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ModelAndView helloWorld() {
+        String message = "<br><div style='text-align:center;'>"
+                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from Spring MVC **********</div><br><br>";
+        return new ModelAndView("welcome", "message", message);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Hello World Spring MVC</title>
+    <style type="text/css">
+        body {
+            background-image: url('http://crunchify.com/bg.png');
+        }
+    </style>
+</head>
+<body>${message}
+
+<br>
+<br>
+<div style="font-family: verdana; padding: 10px; border-radius: 10px; font-size: 12px; text-align:center;">
+
+    Spring MCV Tutorial
+</div>
+</body>
+</html>
